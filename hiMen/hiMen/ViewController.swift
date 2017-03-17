364a0ba50dc7c34ba0f0425023df3f0a1bf90d96
whong7@aliyun.com <souget@gaitedeMBP.lan>
2017-03-17 18:40:22 +0800

    添加项目

---
+++ hiMen/hiMen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hiMen
+//
+//  Created by 盖特 on 2017/3/17.
+//  Copyright © 2017年 盖特. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
